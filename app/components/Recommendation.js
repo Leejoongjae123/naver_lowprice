4fcd30e4cac7c4361718eb07129b4d5828187819
ljj33 <ljj3347@naver.com>
2023-11-02 13:18:59 +0900

    14th

--- app/components/Recommendation.js
+++ app/components/Recommendation.js
@@ -11,63 +11,63 @@ export default function AccessibleTable() {
   return (
     <div className="px-10">
       <div className="relative overflow-x-auto shadow-md rounded-2xl">
-        <table className=" truncate text-sm table-fixed w-full text-left text-gray-500 shadow-xl">
-          <thead className="text-xs md:text-base text-gray-700 uppercase bg-gray-50 font-bold">
+        <table className="truncate text-sm table-fixed w-full text-gray-500 shadow-xl">
+          <thead className="text-center text-xs md:text-base text-gray-700 uppercase bg-gray-50 font-bold">
             <tr>
               <th scope="col" className="px-6 py-3">
                 아파트명
                 
               </th>
               <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">
+                <div className="">
                   평형
                 </div>
               </th>
               <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">
+                <div className="">
                   호가
                 </div>
               </th>
               <th scope="col" className="px-6 py-3">
-                <div className="flex items-center">
+                <div className="">
                   실거래가
                 </div>
               </th>
             </tr>
           </thead>
-          <tbody className="text-xs">
+          <tbody className="text-xs text-center">
             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900  whitespace-normal"
+                className="text-center px-2 py-4 font-medium text-gray-900  whitespace-normal"
               >
                 판교 푸르지오그랑블
               </th>
-              <td className="px-6 py-4">38평형</td>
-              <td className="px-6 py-4">22</td>
-              <td className="px-6 py-4">24</td>
+              <td className=" py-4">38평형</td>
+              <td className=" py-4">22</td>
+              <td className=" py-4">24</td>
             </tr>
             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900  whitespace-normal"
+                className="text-center px-2 py-4 font-medium text-gray-900  whitespace-normal"
               >
                 판교 푸르지오그랑블
               </th>
-              <td className="px-6 py-4">38평형</td>
-              <td className="px-6 py-4">22</td>
-              <td className="px-6 py-4">24</td>
+              <td className=" py-4">38평형</td>
+              <td className=" py-4">22</td>
+              <td className=" py-4">24</td>
             </tr>
             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900  whitespace-normal"
+                className="text-center px-2 py-4 font-medium text-gray-900  whitespace-normal"
               >
                 판교 푸르지오그랑블
               </th>
-              <td className="px-6 py-4">38평형</td>
-              <td className="px-6 py-4">22</td>
-              <td className="px-6 py-4">24</td>
+              <td className=" py-4">38평형</td>
+              <td className=" py-4">22</td>
+              <td className=" py-4">24</td>
             </tr>
           </tbody>
         </table>
